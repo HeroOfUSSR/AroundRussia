88c6a9fe9b005101230a55b354f0b7afc2b4207a
HeroOfUSSR <alexey_ivanov04@mail.ru>
2024-01-27 12:16:18 +0300

    пипяо

--- AroundRussia/Forms/TourOutput.Designer.cs
+++ AroundRussia/Forms/TourOutput.Designer.cs
@@ -28,10 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.pictureTour = new System.Windows.Forms.PictureBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelPrice = new System.Windows.Forms.Label();
+            this.labelTickets = new System.Windows.Forms.Label();
+            this.labelisActual = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureTour)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureTour
+            // 
+            this.pictureTour.Location = new System.Drawing.Point(0, 40);
+            this.pictureTour.Name = "pictureTour";
+            this.pictureTour.Size = new System.Drawing.Size(300, 145);
+            this.pictureTour.TabIndex = 0;
+            this.pictureTour.TabStop = false;
+            this.pictureTour.Click += new System.EventHandler(this.pictureTour_Click);
+            // 
+            // labelName
+            // 
+            this.labelName.Dock = System.Windows.Forms.DockStyle.Top;
+            this.labelName.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelName.Location = new System.Drawing.Point(0, 0);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(300, 37);
+            this.labelName.TabIndex = 1;
+            this.labelName.Text = "Name";
+            this.labelName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.labelisActual);
+            this.panel1.Controls.Add(this.labelTickets);
+            this.panel1.Controls.Add(this.labelPrice);
+            this.panel1.Location = new System.Drawing.Point(0, 182);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(300, 78);
+            this.panel1.TabIndex = 2;
+            // 
+            // labelPrice
+            // 
+            this.labelPrice.Dock = System.Windows.Forms.DockStyle.Top;
+            this.labelPrice.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelPrice.Location = new System.Drawing.Point(0, 0);
+            this.labelPrice.Name = "labelPrice";
+            this.labelPrice.Size = new System.Drawing.Size(300, 37);
+            this.labelPrice.TabIndex = 3;
+            this.labelPrice.Text = "Price";
+            this.labelPrice.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelTickets
+            // 
+            this.labelTickets.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelTickets.Location = new System.Drawing.Point(151, 41);
+            this.labelTickets.Name = "labelTickets";
+            this.labelTickets.Size = new System.Drawing.Size(149, 37);
+            this.labelTickets.TabIndex = 4;
+            this.labelTickets.Text = "Tickets";
+            this.labelTickets.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelisActual
+            // 
+            this.labelisActual.Font = new System.Drawing.Font("Comic Sans MS", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelisActual.Location = new System.Drawing.Point(0, 41);
+            this.labelisActual.Name = "labelisActual";
+            this.labelisActual.Size = new System.Drawing.Size(145, 37);
+            this.labelisActual.TabIndex = 5;
+            this.labelisActual.Text = "isActual";
+            this.labelisActual.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TourOutput
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.pictureTour);
+            this.Name = "TourOutput";
+            this.Size = new System.Drawing.Size(300, 260);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureTour)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private PictureBox pictureTour;
+        private Label labelName;
+        private Panel panel1;
+        private Label labelisActual;
+        private Label labelTickets;
+        private Label labelPrice;
     }
 }
--- AroundRussia/Forms/TourOutput.cs
+++ AroundRussia/Forms/TourOutput.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AroundRussia.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,29 @@ namespace AroundRussia.Forms
 {
     public partial class TourOutput : UserControl
     {
+        private readonly Tour tour;
+
         public TourOutput()
         {
             InitializeComponent();
+        }
+
+        private void InitTour(Tour tour)
+        {
+            labelName.Text = tour.Name;
+            labelPrice.Text = $"{tour.Price}";
+            labelisActual.Text = tour.IsActual ? "Актуален" : "Не актуален";
+            labelisActual.ForeColor = tour.IsActual ? Color.Green : Color.Red;
+            labelTickets.Text = tour.TicketCount.ToString();
+            if (tour.ImagePreview != null)
+            {
+                pictureTour.Image = Image.FromStream(new MemoryStream(tour.ImagePreview));
+            }
+        }
+
+        private void pictureTour_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
